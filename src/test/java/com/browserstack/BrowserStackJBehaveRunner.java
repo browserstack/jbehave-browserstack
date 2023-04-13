95d570a0363bcab9f8b8503226fcf239f9601dc9
Kamalpreet Kaur <kmlkaur73@gmail.com>
2023-04-13 11:01:43 +0530

    chore: test

--- src/test/java/com/browserstack/BrowserStackJBehaveRunner.java
+++ src/test/java/com/browserstack/BrowserStackJBehaveRunner.java
@@ -26,6 +26,7 @@ public class BrowserStackJBehaveRunner {
 
     @Before
     public void setUp() throws Exception {
+        System.out.println("It's a Test!!");
         if (browserstackYamlMap == null) {
             File file = new File(getUserDir() + "/browserstack.yml");
             browserstackYamlMap = convertYamlFileToMap(file, new HashMap<>());
